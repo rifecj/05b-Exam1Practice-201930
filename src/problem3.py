345c02a21f8e7f5390b68ac00ce685eebae3ab14
rifecj <rifecj@rose-hulman.edu>
2019-03-14 09:50:13 -0400

    code

--- src/problem3.py
+++ src/problem3.py
@@ -4,7 +4,7 @@ PRACTICE Exam 1, problem 3.
 Authors: David Mutchler, Vibha Alangar, Matt Boutell, Dave Fisher,
          Mark Hays, Amanda Stouder, Aaron Wilkin, their colleagues,
          and Chloe Rife.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+"""  # done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -96,6 +96,14 @@ def run_test_problem3a():
     print('Test 4 expected:', expected)
     print('       actual:  ', answer)
 
+    # Test 5 (it is on window 3):
+    point = rg.Point(100,150)
+    expected = 16
+    answer = problem3a(window3, point, 4)
+    print()
+    print('Test 5 expected:', expected)
+    print('       actual:  ', answer)
+
     window3.close_on_mouse_click()
 
     # -------------------------------------------------------------------------
@@ -146,6 +154,16 @@ def problem3a(window, point, n):
     #    DIFFICULTY:      7 or 8
     #    TIME ESTIMATE:   20 to 35 minutes.
     # -------------------------------------------------------------------------
+    for k in range (n):
+        start=rg.Point(point.x+20*k,point.y+10*k)
+        end=rg.Point(point.x+20*k,point.y+10*k+50)
+        line = rg.Line(start, end)
+        thick=1+(k)*2
+        if thick>13:
+            thick=13
+        line.thickness=thick
+        line.attach_to(window)
+        window.render()
 
 
 def run_test_problem3b():
